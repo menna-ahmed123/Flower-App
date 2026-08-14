7e9af0f672c462a57ec340bd6324a0d9f40ee03a
HossamEzzat <hossamezzat199@gmail.com>
2026-08-14 21:23:14 +0300

    fix review and finish signup

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -1,3 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:flower_app/core/network/safe_call.dart';
+import 'package:flower_app/features/auth/register/api/dio_register_api.dart';
+import 'package:flower_app/features/auth/register/data/data_sources/register_remote_data_source.dart';
+import 'package:flower_app/features/auth/register/data/data_sources/register_remote_data_source_impl.dart';
+import 'package:flower_app/features/auth/register/data/repositories/register_repository_impl.dart';
+import 'package:flower_app/features/auth/register/domain/repositories/register_repository.dart';
+import 'package:flower_app/features/auth/register/domain/use_cases/register_use_case.dart';
+import 'package:flower_app/features/auth/register/domain/use_cases/register_use_case_impl.dart';
+import 'package:flower_app/features/auth/register/domain/validators/register_form_validator.dart';
+import 'package:flower_app/features/auth/register/presentation/view_model/register_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'di.config.dart';
@@ -8,4 +19,51 @@ final getIt = GetIt.instance;
   preferRelativeImports: true, // default
   asExtension: true, // default
 )
-Future <void> configureDependencies() async => getIt.init();
+Future <void> configureDependencies() async {
+  getIt.init();
+  _registerSignupDependencies();
+}
+
+void _registerSignupDependencies() {
+  if (getIt.isRegistered<RegisterBloc>()) return;
+  _registerSignupValidator();
+  _registerSignupApi();
+  _registerSignupData();
+  _registerSignupBloc();
+}
+
+void _registerSignupValidator() {
+  getIt.registerFactory<RegisterFormValidator>(
+    () => const RegisterFormValidator(),
+  );
+}
+
+void _registerSignupApi() {
+  getIt.registerLazySingleton<DioRegisterApi>(
+    () => DioRegisterApi(getIt<Dio>()),
+  );
+}
+
+void _registerSignupData() {
+  getIt.registerFactory<RegisterRemoteDataSource>(
+    () => RegisterRemoteDataSourceImpl(getIt<DioRegisterApi>()),
+  );
+  getIt.registerFactory<RegisterRepository>(
+    () => RegisterRepositoryImpl(
+      getIt<RegisterRemoteDataSource>(),
+      getIt<SafeCall>(),
+    ),
+  );
+  getIt.registerFactory<RegisterUseCase>(
+    () => RegisterUseCaseImpl(getIt<RegisterRepository>()),
+  );
+}
+
+void _registerSignupBloc() {
+  getIt.registerFactory<RegisterBloc>(
+    () => RegisterBloc(
+      getIt<RegisterUseCase>(),
+      getIt<RegisterFormValidator>(),
+    ),
+  );
+}
