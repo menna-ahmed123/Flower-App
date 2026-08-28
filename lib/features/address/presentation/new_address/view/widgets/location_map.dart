c0f6988fdc3b4c8f961182a2cfb4f6f1f94a6536
joud <joudysami13@gmail.com>
2026-08-28 23:12:20 +0300

    refactor(location_map): clean up code and remove commented-out polygons

--- lib/features/address/presentation/new_address/view/widgets/location_map.dart
+++ lib/features/address/presentation/new_address/view/widgets/location_map.dart
@@ -29,8 +29,7 @@ class _LocationMapState extends State<LocationMap> {
 
   @override
   Widget build(BuildContext context) {
-      final apiKey = dotenv.env['MAPTILER_API_KEY'];
-
+    final apiKey = dotenv.env['MAPTILER_API_KEY'];
     return FlutterMap(
       options: MapOptions(
         initialCenter: widget.initialLocation,
@@ -39,36 +38,16 @@ class _LocationMapState extends State<LocationMap> {
           setState(() {
             selectedLocation = point;
           });
-
           widget.onLocationSelected?.call(point);
         },
       ),
       children: [
-     
-TileLayer(
-  urlTemplate:
-      'https://api.maptiler.com/maps/streets-v4/{z}/{x}/{y}.png?key=$apiKey&language=en',
-      // 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-
+        TileLayer(
+          urlTemplate:
+              'https://api.maptiler.com/maps/streets-v4/{z}/{x}/{y}.png?key=$apiKey&language=en',
+          // 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
           userAgentPackageName: 'com.example.flower_app',
         ),
-        //====== for select my store======//
-        // PolygonLayer(
-        //     polygons: [
-        //       Polygon(
-        //         points: const [
-        //           LatLng(30.0500, 31.2300),
-        //           LatLng(30.0600, 31.2400),
-        //           LatLng(30.0500, 31.2500),
-        //           LatLng(30.0400, 31.2450),
-        //           LatLng(30.0350, 31.2350),
-        //         ],
-        //         color: Colors.pink.withOpacity(0.3),
-        //         borderColor: Colors.pink,
-        //         borderStrokeWidth: 3,
-        //       ),
-        //     ],
-        //   ),
         MarkerLayer(
           markers: selectedLocation == null
               ? []
